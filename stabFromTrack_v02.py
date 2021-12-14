d422a89c27e24daa1abe1c8cdb38fce832c19392
Thomas Hollier <hollierthomas@gmail.com>
2021-12-13 17:12:28 -0800

    use in and out from track data when no frame given

--- stabFromTrack_v02.py
+++ stabFromTrack_v02.py
@@ -6,8 +6,9 @@ import numpy as np
 
 
 parser = argparse.ArgumentParser(description="Stabilize a movie file based on values in a data file in the following format:\n\tFRAME X Y.  \nFRAME is the frame number integer, X and Y are normalized floats. Blender\'s Y coordinate is flipped so if image has the same orientation in blender as it does in openCV view, yFlip argument is needed")
-parser.add_argument('-fs', '--frameStart', default=1, type=int, dest='startFrame', help='first frame of the stabilize')  
-parser.add_argument('-fe', '--frameEnd',default=1, type=int, dest='endFrame', help='first frame of the stabilize')   
+
+parser.add_argument('-fs', '--frameStart', default=-1, type=int, dest='frameStart', help='first frame of the stabilize')  
+parser.add_argument('-fe', '--frameEnd',default=-1, type=int, dest='frameEnd', help='first frame of the stabilize')   
 parser.add_argument('-td', '--trackData',default=1, type=str, dest='trackData', help='frame, X and Y values')   
 parser.add_argument('-xf', '--xFlip', action='store_true', dest='xFlip', help='invert the x value in case image was tracked with horizontal flip')   
 parser.add_argument('-xr', '--xReverse', action='store_true',dest='xReverse', help='')   
@@ -23,8 +24,8 @@ parser.add_argument('inputMovie', help='input movie')
 
 args = parser.parse_args()
 
-startFrame = args.startFrame
-endFrame = args.endFrame
+frameStart = args.frameStart
+frameEnd = args.frameEnd
 writeOutput = args.writeOutput
 trackData = args.trackData
 xFlip = args.xFlip
@@ -39,13 +40,11 @@ inputMovie = args.inputMovie
 patt = re.compile('(.mov)|(.MP4)|(.MOV)|(.mp4)')
 outputMovieFile = re.sub(patt, '_%s.mp4' % token, inputMovie)
 
-if refFrame == -1:
-	refFrame = startFrame
 
 print("\n")
 print("inputMovie: %s" % inputMovie)
-print("startFrame: %s" % startFrame)
-print("endFrame: %s" % endFrame)
+print("frameStart: %s" % frameStart)
+print("frameEnd: %s" % frameEnd)
 print("xFlip: %s" % xFlip)
 print("xReverse: %s" % xReverse)
 print("yFlip: %s" % yFlip)
@@ -72,6 +71,10 @@ def readTrackerData(myFile):
 	with open(myFile) as file:
 		tracker = file.readlines()
 
+	frameFirst = int(tracker[0].split()[0])
+	frameLast  = int(tracker[-1:][0].split()[0])
+	clipRange = frameLast - frameFirst
+	fileRange = len(tracker)
 	# create dictionary of x y transforms per frame
 	trackerDict = {}
 	for v in tracker:
@@ -86,7 +89,19 @@ def readTrackerData(myFile):
 			y = float(y)
 		trackerDict[k]=(x,y)
 
-	return trackerDict
+	# fill in missing frames by copying last value
+	n = frameFirst
+	latest = 0
+	while n < frameLast:
+		if str(n) in trackerDict.keys():
+			latest = n
+		else:
+			print("missing frame %s in track file, using frame %s" % (n, latest))
+			trackerDict[str(n)] = trackerDict[str(latest)]
+		n = n + 1
+
+
+	return frameFirst, frameLast, trackerDict
 
 def getMatrix(trackerDict, frame, frameRef, w, h):
 	v = trackerDict[str(frame)]
@@ -157,25 +172,37 @@ outputMovie = cv2.VideoWriter(
 	(movieWidth,movieHeight)
 ) if writeOutput else None
 
+
+#read from trackign data file
+ff, lf, trackingData = readTrackerData(trackData)
+
+if frameStart == -1:
+	frameStart = ff
+if frameEnd == -1:
+	frameEnd = lf
+if refFrame == -1:
+	refFrame = frameStart
+
 #--- Go to first specified frame
-frameRange = get_movie_range(movie)
-jump_to_frame(startFrame, movie)
+jump_to_frame(frameStart, movie)
 
+i = frameStart
 
-trackingData = readTrackerData(trackData)
-
-i = startFrame
-
-while i < endFrame:
+startTime = time.perf_counter()
+frameRange = frameEnd - frameStart
+frameCurrent = frameStart
+while frameCurrent < frameEnd:
 
 	ret, srcFrame = read_frame(movie)
 	if ret != True:
 		print("Problem reading frame %s"%i)
 		exit
 
-	mtx = getMatrix(trackingData, i+1, refFrame, movieWidth, movieHeight)
-	x,y = getXY(trackingData, i+1, movieWidth, movieHeight)
+	mtx = getMatrix(trackingData, frameCurrent+1, refFrame, movieWidth, movieHeight)
+	x,y = getXY(trackingData, frameCurrent+1, movieWidth, movieHeight)
 	if not debug:
+		#cv2.circle(srcFrame,(x,y),30,(100,0,255),-1)
+		
 		dstStab = cv2.warpAffine(srcFrame, mtx, (movieWidth,movieHeight))
 		dstStabShow = cv2.resize(dstStab,(int(movieWidth*.25),int(movieHeight*.25)))
 		for x in range(50,dstStabShow.shape[0],50):
@@ -183,16 +210,23 @@ while i < endFrame:
 				cv2.circle(dstStabShow,(y,x),3,(255,0,0),-1)
 		cv2.imshow("stab",  dstStabShow)
 	else:
-		cv2.circle(srcFrame,(x,y),10,(255,0,0),-1)
+		cv2.circle(srcFrame,(x,y),30,(100,0,255),-1)
 		cv2.imshow("stab",  cv2.resize(srcFrame,(int(movieWidth*.25),int(movieHeight*.25))))
 		dstStab = srcFrame
 
 	outputMovie.write(dstStab) if writeOutput else None
 
-	sys.stdout.write("\rframe %4d of %s; %04f%% complete" % (i-startFrame, endFrame-startFrame, 100.0*float(i-startFrame)/float(endFrame-startFrame)))
+	frameCurrent = frameCurrent + 1
+
+	percentDone = 100.0*float(frameCurrent-frameStart)/float(frameRange)
+	elapsedTime = time.perf_counter()-startTime
+	remainingTime = int(.99+elapsedTime/percentDone * (100-percentDone) )
+	remainingTime = "%02d:%02d" % (int(remainingTime/60), int(remainingTime%60))
+	sys.stdout.write("\rFrame %4d of %s -- %.02f%% complete -- %s seconds remaining" % (frameCurrent-frameStart, frameRange, percentDone, remainingTime))
 	sys.stdout.flush()
 
-	i += 1
+
+
 	# Exit if ESC pressed
 	k = cv2.waitKey(1) & 0xff
 	if k == 27 : break
