4a30358a3faba1d5aa8125b141834d09339b21af
Thomas Hollier <hollierthomas@gmail.com>
2021-12-17 13:34:24 -0800

    Cleaned up frame in/out point code

--- frameavg.py
+++ frameavg.py
@@ -11,7 +11,7 @@ import argparse, sys, time
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-lut', '--lut', default=None, type=str, dest='lut', help='path to lut file to apply')
 
-parser.add_argument('-p', '--prefix', default="", type=str, dest='prefix', help='prefix for output file name')
+parser.add_argument('-p', '--prefix', default="basename", type=str, dest='prefix', help='prefix for output file name')
 parser.add_argument('-fs', '--frameStart', default=-1, type=int, dest='frameStart', help='first frame of the stabilize')
 parser.add_argument('-fe', '--frameEnd', default=-1, type=int, dest='frameEnd', help='first frame of the stabilize')
 parser.add_argument('-fo', '--frameOffset', default=0, type=int, dest='frameOffset', help='frame start offset')
@@ -81,22 +81,11 @@ print("claheSize: %s" % claheGrid)
 print("claheBefore: %s" % claheBefore)
 print("token: %s" % token)
 print("output: %s\n" % output)
-if frameStart == -1 and frameEnd == -1:
-	print("Frame start and end not specified. Using all frames in clip.\n") 
 
 
-
-cap = cv2.VideoCapture(inputMovie)
-inputNumberOfFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
-cap.set(cv2.CAP_PROP_POS_FRAMES,frameOffset)
-
-if frameStart == -1:
-	frameStart = 1
-if frameEnd == -1:
-	frameEnd = inputNumberOfFrames - 1
-
-
-
+####
+#### define color correction luts
+####
 def softContrastCalc(x,k):
 	x1 = .5*pow(2*x,k)
 	x2 = .5*pow(2*(1-x),k)
@@ -131,21 +120,33 @@ def makeXformLUT():
 	return clut
 
 
-# take first frame of the video
-n=1.0
-ret,frame = cap.read()
-buff=np.float32(frame)/255.
+if softContrast != 1:
+	clut = makeContrastLUT()
+if lut:
+	xformLut = makeXformLUT()
 
+#######
+# work out in out frames
+######
 
-startTime = time.perf_counter()
+cap = cv2.VideoCapture(inputMovie)
+inputNumberOfFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
 
-clut = makeContrastLUT()
-xformLut = makeXformLUT()
-
-
-startTime = time.perf_counter()
+if frameStart == -1 and frameEnd == -1:
+	print("Frame start and end not specified. Using all frames in clip.\n") 
+	frameStart = 1
+	frameEnd = inputNumberOfFrames - 1
 frameRange = frameEnd - frameStart
 frameCurrent = frameStart
+
+# take first frame of the video
+cap.set(cv2.CAP_PROP_POS_FRAMES,frameCurrent)
+ret,frame = cap.read()
+buffer=np.float32(frame)/255.
+
+
+startTime = time.perf_counter()
+
 while frameCurrent < frameEnd:
 	ret,frame = cap.read()
 	if clahe and claheBefore:
@@ -169,10 +170,10 @@ while frameCurrent < frameEnd:
 	if lut:
 		frameData = cv2.LUT((frameData*255).astype(np.uint8), xformLut).astype(np.float32)/255
 		frameData = np.clip(frameData,0,1)
-	buff = buff + frameData
+	buffer = buffer + frameData
 	frameCurrent = frameCurrent + 1
 
-	percentDone = 100.0*float(frameCurrent)/float(frameRange)
+	percentDone = 100.0*float(frameCurrent-frameStart)/float(frameRange)
 	elapsedTime = time.perf_counter()-startTime
 	remainingTime = int(.99+elapsedTime/percentDone * (100-percentDone) )
 	remainingTime = "%02d:%02d" % (int(remainingTime/60), int(remainingTime%60))
@@ -181,31 +182,31 @@ while frameCurrent < frameEnd:
 
 
 if not add:
-	buff = buff/(frameRange)
-	#buff = cv2.LUT((buff*255).astype(np.uint8), clut).astype(np.float32)/255.0
+	buffer = buffer/(frameRange)
+	#buffer = cv2.LUT((buffer*255).astype(np.uint8), clut).astype(np.float32)/255.0
 	if bright != 1:
-		buff *= bright
+		buffer *= bright
 	if gamma != 1 and not gammaBefore:
-		buff = pow(buff,1/gamma)
+		buffer = pow(buffer,1/gamma)
 	if clahe and not claheBefore:
 		print("doing the clahe")
-		buff = buff * 65535
-		buff = buff.astype(np.uint16) 
-		r = buff[:,:,0]
-		g = buff[:,:,1]
-		b = buff[:,:,2]
+		buffer = buffer * 65535
+		buffer = buffer.astype(np.uint16) 
+		r = buffer[:,:,0]
+		g = buffer[:,:,1]
+		b = buffer[:,:,2]
 		clahe = cv2.createCLAHE(clipLimit=claheClip, tileGridSize=(claheGrid,claheGrid))
 		r = clahe.apply(r)
 		g = clahe.apply(g)
 		b = clahe.apply(b)
-		buff = cv2.merge([r,g,b])	
-		buff = buff.astype(np.float32)
-		buff = buff/65535
-		buff = np.clip(buff,0,1)
+		buffer = cv2.merge([r,g,b])	
+		buffer = buffer.astype(np.float32)
+		buffer = buffer/65535
+		buffer = np.clip(buffer,0,1)
 	
-buff = np.clip(buff * 65535, 0, 65535)
-buff = buff.astype(np.uint16)
-cv2.imwrite(output,buff)
+buffer = np.clip(buffer * 65535, 0, 65535)
+buffer = buffer.astype(np.uint16)
+cv2.imwrite(output,buffer)
 
 cap.release()
 
