e78b08b9f201e37ed44ac041da14be6497a3eb63
Thomas Hollier <hollierthomas@gmail.com>
2022-01-07 00:07:25 -0800

    adding multi track curves

--- blenderKeyframeExport.py
+++ blenderKeyframeExport.py
@@ -1,42 +1,49 @@
-prefix='11222021170021'
-movie='%s.mov' % prefix
-track='%s_track_01' % prefix
-filepath='/Users/thomas/Documents/slitscans/20211122_pier/%s_track_01.crv' % prefix
+import re, bpy
 
-# movie clip that the track is done to
-clip = bpy.data.movieclips[movie]
-# track name in blender
-track = clip.tracking.tracks[track]
-# markers are collections of markers which contain the per-frame info
-markers=track.markers
-
-f = open(filepath, 'w')
-for k,v in markers.items():
-	s = ("%s %s %s\n"% (v.frame,v.co[0],v.co[1]))
-	f.write(s)
-
-f.close()
-print("%s done")
+patt = re.compile('(.mov)|(.MP4)|(.MOV)|(.mp4)')
+p2 = re.compile('(track[0-9]{2})_([0-9]{2})')
+testTrackName = 'track02'
+m = re.match(p2, testTrackName)
+if m and len(m.groups()) == 2:
+	n = m.group(1)
+	nb = m.group(2)
+	print("name:", n, "number:", nb)
+else:
+	print(testTrackName)
 
 
-for clip in [bpy.data.movieclips['IMG_2023.mov']]:
+print("hello")
+
+p2 = re.compile('(track[0-9]{2})_([0-9]{2})')
+
+for clip in bpy.data.movieclips:
+	# dictionary of all the tracks for a clip
+	trackDict = {}
 	for track in clip.tracking.tracks:
-		filepath = clip.filepath
-		filename = filepath.replace('.mov', '_%s.crv'%track.name)
-		filename = "/Users/thomas/Documents/slitscans/20210828_pierLights/%s" % filename
-		f = open(filename, 'w')
-		for k,v in track.markers.items():
-	        	s = ("%s %s %s\n"% (v.frame,v.co[0],v.co[1]))
-        		f.write(s)
-		f.close()
+		match = re.match(p2, track.name)
+		if match and len(match.groups()) == 2:
+			if match.group(1) in trackDict.keys():
+				trackDict[match.group(1)].append(track.name)
+			else:
+				trackDict[match.group(1)] = [track.name]
+		else:
+			trackDict[track.name] = track.name
+			filepath = clip.filepath
+			filename = re.sub(patt, '_%s.crv'%track.name, filepath)
+			filename = "/Users/thomas/Documents/slitscans/20211227_parrishClouds/%s" % filename
+			f = open(filename, 'w')
+			for k,v in track.markers.items():
+				s = ("%s %s %s\n"% (v.frame,v.co[0],v.co[1]))
+				ret = f.write(s)
+			f.close()
+	print("%s:\n\t%s\n" % (clip.name, trackDict))
 
 
 
 
 
+'''
 
-
-'
 # Below was a technique that required tracks to be 
 # baked on null object animation
 # not a good way...
