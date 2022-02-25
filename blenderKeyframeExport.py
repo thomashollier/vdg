3b036837bfaa31f034db854f79d78b7565c772b7
Thomas Hollier <hollierthomas@gmail.com>
2022-02-24 16:09:38 -0800

    added flipping x/y for portrait movies in blender

--- blenderKeyframeExport.py
+++ blenderKeyframeExport.py
@@ -1,67 +1,57 @@
 import re, bpy
 
+def reformatTrackData(trackObject, fileObject, offsetXY, IS_LANDSCAPE):
+    # check the logic of IS_LANDSCAPE when using 
+    # already swapped X and Y offsets
+    trackStartLocation = list(trackObject.markers.values())[0].co
+    if offsetXY != (0,0):
+        xOffset = trackStartLocation[0]-offsetXY[0]
+        yOffset = trackStartLocation[1]-offsetXY[1]
+    else:
+        xOffset = 0
+        yOffset = 0
+    for k,v in trackObject.markers.items():
+        if IS_LANDSCAPE:
+            s = ("%s %s %s\n"% (v.frame,v.co[0]-xOffset,v.co[1]-yOffset))
+        else:
+            s = ("%s %s %s\n"% (v.frame,v.co[1]-xOffset,v.co[0]-yOffset))
+        ret = fileObject.write(s)
+    if IS_LANDSCAPE:
+        return (v.co[0]-xOffset,v.co[1]-yOffset)
+    else:
+        return (v.co[1]-xOffset,v.co[0]-yOffset)
+    
+
+multiTrackPatt = re.compile('(track[0-9]{2})_([0-9]{2})')
 patt = re.compile('(.mov)|(.MP4)|(.MOV)|(.mp4)')
-p2 = re.compile('(track[0-9]{2})_([0-9]{2})')
-testTrackName = 'track02'
-m = re.match(p2, testTrackName)
-if m and len(m.groups()) == 2:
-	n = m.group(1)
-	nb = m.group(2)
-	print("name:", n, "number:", nb)
-else:
-	print(testTrackName)
-
-
-print("hello")
-
-p2 = re.compile('(track[0-9]{2})_([0-9]{2})')
 
 for clip in bpy.data.movieclips:
-	# dictionary of all the tracks for a clip
-	trackDict = {}
-	for track in clip.tracking.tracks:
-		match = re.match(p2, track.name)
-		if match and len(match.groups()) == 2:
-			if match.group(1) in trackDict.keys():
-				trackDict[match.group(1)].append(track.name)
-			else:
-				trackDict[match.group(1)] = [track.name]
-		else:
-			trackDict[track.name] = track.name
-			filepath = clip.filepath
-			filename = re.sub(patt, '_%s.crv'%track.name, filepath)
-			filename = "/Users/thomas/Documents/slitscans/20211227_parrishClouds/%s" % filename
-			f = open(filename, 'w')
-			for k,v in track.markers.items():
-				s = ("%s %s %s\n"% (v.frame,v.co[0],v.co[1]))
-				ret = f.write(s)
-			f.close()
-	print("%s:\n\t%s\n" % (clip.name, trackDict))
+    # dictionary of all the tracks for a clip
+    if clip.size[0]>clip.size[1]:
+        IS_LANDSCAPE=True
+    else:
+        IS_LANDSCAPE=False
+    trackDict = {}
+    for track in clip.tracking.tracks:
+        match = re.match(multiTrackPatt, track.name)
+        if match and len(match.groups()) == 2:
+            if match.group(1) in trackDict.keys():
+                trackDict[match.group(1)].append(track)
+            else:
+                trackDict[match.group(1)] = [track]
+        else:
+            trackDict[track.name] = track
+    for k, v in trackDict.items():
+        filepath = "%s/%s_%s.crv"%(bpy.path.abspath('//'), re.sub(patt, "", clip.name), k)
+        print(filepath)
+        f = open(filepath, 'w')
+        offset = (0,0)
+        if isinstance(v, list):
+            for t in v:
+                offset = reformatTrackData(t, f, offset, IS_LANDSCAPE)
+        else:
+            reformatTrackData(v, f, (0,0), IS_LANDSCAPE)
+        f.close()
 
 
-
-
-
-'''
-
-# Below was a technique that required tracks to be 
-# baked on null object animation
-# not a good way...
-
-action = bpy.data.actions["track4741_01Action"]
-
-xCurves=action.fcurves[0]
-yCurves=action.fcurves[2]
-
-xKeys = xCurves.keyframe_points
-yKeys = yCurves.keyframe_points
-
-f = open('/Users/thomas/Documents/slitscans/20211127_newyork1/track4741_01', 'w')
-
-for xk, yk in zip(xKeys, yKeys):
-	s = "%s %s %s\n" % (int(xk.co[0]), xk.co[1], yk.co[1])
-	f.write(s)
-
-f.close()
-'''
 
